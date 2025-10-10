caad90595fbf338c081519beff0db71220e79d57
raj9169 <rajkushwaha9169@gmail.com>
2025-10-10 10:58:01 +0530

    new db code

--- dbConnection.php
+++ dbConnection.php
@@ -1,14 +1,4 @@
 <?php
-$con = new mysqli("localhost", "osms_user", "secure", "osms_db", 3306);
-
-if ($con->connect_error) {
-    echo "Error: " . $con->connect_error;
-} else {
-    echo "Connected!";
-    $con->close();
-}
-?>
-<?php
 // Database configuration
 $db_host = "localhost";
 $db_user = "osms_user";
@@ -17,8 +7,6 @@ $db_name = "osms_db";
 $db_port = 3306;
 
 echo "Testing database connection...<br>";
-
-
 
 // Create connection
 $con = new mysqli($db_host, $db_user, $db_password, $db_name, $db_port);
