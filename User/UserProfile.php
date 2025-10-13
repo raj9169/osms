98de9e83b7fa913d0368efdfc9aa51a91f6bae39
Raj Kushwaha <83691047+raj9169@users.noreply.github.com>
2025-10-13 10:00:10 +0530

    Update UserProfile.php

--- User/UserProfile.php
+++ User/UserProfile.php
@@ -8,7 +8,7 @@ ini_set('display_errors', 1);
 session_start();
 if (!defined('TITLE')) define('TITLE', 'User Profile');
 if (!defined('PAGE')) define('PAGE', 'UserProfile');
-include_once('../includes/header.php');
+include_once('./includes/header.php');
 include('../dbConnection.php');
 
 // Session security
