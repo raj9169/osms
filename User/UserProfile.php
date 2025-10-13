a579c49c0e5274ea0642f1aadace40a2cfdba0ff
Raj Kushwaha <83691047+raj9169@users.noreply.github.com>
2025-10-13 16:28:01 +0530

    Update UserProfile.php

--- User/UserProfile.php
+++ User/UserProfile.php
@@ -62,3 +62,5 @@ if (isset($_REQUEST['nameupdate'])) {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
+<?php include('includes/footer.php'); ?>
