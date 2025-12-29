9b6f6f6e8b54eb620c21af37318191146547240d
Raj Kushwaha <83691047+raj9169@users.noreply.github.com>
2025-12-29 18:51:02 +0530

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ echo "PHP is working!<br>";
 echo "Current time: " . date('Y-m-d H:i:s') . "<br>";
 
 // Test database connection
-$con = new mysqli("localhost", "osms_user", "secure", "osms_db", 3306);
+$con = new mysqli("tryit-thisdb.c6bc06igsqpj.us-east-1.rds.amazonaws.com", "admin", "Raj9918264088", "osms_db", 3306);
 
 if ($con->connect_error) {
     echo "❌ Database Error: " . $con->connect_error . "<br>";
